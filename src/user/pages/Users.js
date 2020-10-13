df2ef0202fe616fe4f20c49a49204ea440fb2599
David Martin <david.martin@hcss.com>
2020-10-13 09:33:10 -0500

    commit on 'master'

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -9,7 +9,7 @@ const Users = () => {
         {
             id: 'u1',
             name: 'David Martin',
-            image: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwompampsupport.azureedge.net%2Ffetchimage%3FsiteId%3D7575%26v%3D2%26jpgQuality%3D100%26width%3D700%26url%3Dhttps%253A%252F%252Fi.kym-cdn.com%252Fentries%252Ficons%252Ffacebook%252F000%252F013%252F564%252Fdoge.jpg&imgrefurl=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fdoge&tbnid=P8AvGsIcs7xgeM&vet=12ahUKEwjK4ryA07HsAhWQBFMKHTsKCL4QMygAegUIARDYAQ..i&docid=cbfcBh86HZuuEM&w=700&h=394&q=doge&ved=2ahUKEwjK4ryA07HsAhWQBFMKHTsKCL4QMygAegUIARDYAQ',
+            image: 'www.google.com/imgres?imgurl=https%3A%2F%2Fwompampsupport.azureedge.net%2Ffetchimage%3FsiteId%3D7575%26v%3D2%26jpgQuality%3D100%26width%3D700%26url%3Dhttps%253A%252F%252Fi.kym-cdn.com%252Fentries%252Ficons%252Ffacebook%252F000%252F013%252F564%252Fdoge.jpg&imgrefurl=https%3A%2F%2Fknowyourmeme.com%2Fmemes%2Fdoge&tbnid=P8AvGsIcs7xgeM&vet=12ahUKEwjK4ryA07HsAhWQBFMKHTsKCL4QMygAegUIARDYAQ..i&docid=cbfcBh86HZuuEM&w=700&h=394&q=doge&ved=2ahUKEwjK4ryA07HsAhWQBFMKHTsKCL4QMygAegUIARDYAQ',
             places: 3
         }
     ]; //In all caps because it's a 'dummy' array
